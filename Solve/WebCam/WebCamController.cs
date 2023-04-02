1c3d62d54dd7414285a7b0609f5a3144c32b142f
Diogo Manhaes <dsmanhaes@gmail.com>
2023-04-02 14:51:47 -0300

    refactor: treats errors and improves the way to handle with webcams

--- Solve/WebCam/WebCamController.cs
+++ Solve/WebCam/WebCamController.cs
@@ -4,17 +4,26 @@ namespace Solve
 {
   namespace WebCam
   {
+    using Debug;
+
     public class WebCamController : MonoBehaviour
     {
       public int camWidth;
       public int camHeight;
       public int camFPS;
+      public bool mirrored;
       void Start()
       {
+        if (camWidth == 0 || camHeight == 0 || camFPS == 0)
+          DebugController.Error(typeof(WebCamController), "Some parameter isn't set");
+        float aspect = (float)camWidth / (float)camHeight * ((mirrored)? -1: 1);
+        Vector3 scale = new Vector3(aspect, 1, 1);
+        transform.localScale = scale;
         WebCamTexture webcamTexture = new WebCamTexture(camWidth, camHeight, camFPS);
         Renderer renderer = GetComponent<Renderer>();
         renderer.material.mainTexture = webcamTexture;
         webcamTexture.Play();
+        DebugController.Log(typeof(WebCamController), "Webcam initialized");
       }
     }
   }
